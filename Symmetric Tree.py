b691afbe1771e62f8446dedc1837a09d899e8b19
Sanjay K J <20cse0173@vvce.ac.in>
2025-02-22 13:06:05 +0530

    Symmetric Tree code

--- Symmetric Tree.py
+++ Symmetric Tree.py
@@ -4,3 +4,26 @@
 # Input: root = [1,2,2,3,4,4,3]
 # Output: true
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def same(root1,root2):
+            if not root1 and not root2:
+                return True
+
+            if not root1 or not root2:
+                return False
+
+            if root1.val != root2.val:
+                return False
+                
+            return same(root1.left, root2.right) and \
+                   same(root1.right, root2.left)
+
+        return same(root, root)
+             
